ab232ef553872d837fbfbb5f1a87cc2c8722dfbd
Agsikap3D <paolomiguelnaty14@gmail.com>
2025-04-04 01:47:50 +0800

    Initial Commit
    Added Files

---
+++ Assets/Player_Movement.cs
@@ -0,0 +1,71 @@
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float Force_Movement = 1000f;
+    public float Force_Jump = 10000f;
+    private bool Key_Press = false;
+    private bool Grounded = false;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Grounded = true;
+
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Grounded = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !Key_Press && Grounded)
+        {
+            Key_Press = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Key_Press = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+ 
+        if (Input.GetKey("w") && Grounded)
+        {
+            Debug.Log("W is pressed");
+            rb.AddForce(0,0,Force_Movement * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s") && Grounded)
+        {
+            rb.AddForce(0,0,-Force_Movement * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d") && Grounded)
+        {
+            rb.AddForce(Force_Movement * Time.deltaTime, 0,0);
+        }
+
+        if (Input.GetKey("a") && Grounded)
+        { 
+            rb.AddForce(-Force_Movement * Time.deltaTime,0,0);
+        }
+
+        if (Key_Press)
+         {
+            rb.AddForce(0, Force_Jump * Time.deltaTime, 0);
+            Key_Press=false;
+        }
+
+
+    }
+}
